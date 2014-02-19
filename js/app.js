18d26f1594ff33332100d1e8f4c3cef80929394c
Petri Partanen <petri.j.partanen@metropolia.fi>
2014-02-19 14:48:41 +0200

    Added routing

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var batman = angular.module('flaming-batman', ['ngRoute']);
+
+batman.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl:'home.html'
+		})
+		.when('/batman', {
+			templateUrl:'batman.html'
+		})
+		.when('/cool-facts', {
+			templateUrl:'cool-facts.html'
+		})
+		.when('/enemies', {
+			templateUrl:'enemies.html'
+		})
+		.otherwise({
+			redirectTo:'/'
+		});
+});
